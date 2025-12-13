b780cbf1079d209b124fd4f62fca396d91eb4137
Danh0DIY <dnn345169@gmail.com>
2025-12-13 18:53:37 +0700

    Update save.php

--- save.php
+++ save.php
@@ -1,1 +1,9 @@
+<?php
+require "connect.php";
+
+$content = mysqli_real_escape_string($conn, $_POST['content']);
+$conn->query("INSERT INTO test_data (content) VALUES ('$content')");
+
+header("Location: index.php");
+?>
 
